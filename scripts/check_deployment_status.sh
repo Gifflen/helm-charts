a3612a3eac437b63cab268bfaf8cc03fc7d1dd5d
Sameer Garg <sameer.garg@sony.com>
2018-07-11 12:42:17 +0200

    Adding dummy scipt for checking deployment status

---
+++ scripts/check_deployment_status.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#This is a place holder for now, to get the pipeline workflow going
+ 
+echo "*** Poll the kubernetes cluster and check to see if the pod/s are up ***"
+exit 0
